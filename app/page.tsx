079fbf6442dcd330132c918f310c70e968cae9f9
devinscott25 <devintscott25@gmail.com>
2024-11-15 19:41:21 -0500

    Changed pricing and features on cards

--- app/page.tsx
+++ app/page.tsx
@@ -67,7 +67,7 @@ const pricingPlans = [
     priceId: "price_pro_id",
     features: [
       "Health Assistant",
-      "Financial Advisor",
+      "Financial Advisor" ,
     ],
     popular: true,
   },
